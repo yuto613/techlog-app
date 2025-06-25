39635f15dfff52d4ec83087b14b33fc99d6b1a0b
大津由跳 <yutootsu@ootsuyoshitobes-MacBook-Pro.local>
2025-06-25 12:45:08 +0900

    不要ファイルが自動生成されない設定

--- config/application.rb
+++ config/application.rb
@@ -2,26 +2,18 @@ require_relative "boot"
 
 require "rails/all"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
+
 Bundler.require(*Rails.groups)
 
 module TechlogApp
-  class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+  class Application < Rails::Application 
     config.load_defaults 8.0
-
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g| 
+      g.assets false          
+      g.helper false      # helperファイルを自動生成しない
+      g.test_framework :rspec # テストフレームワークをrspecに設定
+    end  
   end
 end
